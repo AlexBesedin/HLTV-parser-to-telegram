0c5b92e62f2252a4542bbd9e0aab47ef8d5e03f1
Alex Besedin <alebesedin@yandex.ru>
2023-07-01 14:51:36 +0300

    правки по коду, правка README

--- README.md
+++ README.md
@@ -7,8 +7,6 @@
 - **aiohttp**: для работы с сетевыми запросами.
 - **beautifulsoup4**: для парсинга HTML и XML.
 - **pyTelegramBotAPI**: для создания Telegram-бота.
-- **requests**: для выполнения HTTP-запросов.
-- **requests-cache**: для кеширования HTTP-ответов.
 - **docker** и **docker-compose**: для контейнеризации приложения с использованием Docker.
 
 ## Как запустить проект:
@@ -17,33 +15,15 @@
 git clone git@github.com:AlexBesedin/HLTV-parser-to-telegram.git
 cd HLTV-parser-to-telegram
 ```
-Cоздать и активировать виртуальное окружение:
-```sh
-python3 -m venv env
-```
-Если у вас Linux/macOS:
-```sh
-source env/bin/activate
-```
-Если у вас windows
-```sh
-source env/scripts/activate
-```
-```sh
-python3 -m pip install --upgrade pip
-```
-Установить зависимости из файла requirements.txt:
-```sh
-python -m pip install -r requirements.txt
-```
+
 Создать файл с переменными окружения .env
 ```sh
 touch .env
 ```
 Пример содержимого файла .env:
 ```sh
-TELEGRAM_TOKEN =Y1235124643414645513125
-CHAT_ID =-41281282371851923981293
+TELEGRAM_TOKEN=Y1235124643414645513125
+CHAT_ID=-41281282371851923981293
 ```
 Установите docker и docker-compose:
 ```sh
--- main/main.py
+++ main/main.py
@@ -1,38 +1,42 @@
 import asyncio
 import aiohttp
-import requests_cache
 import random
 import logging
 from urllib.parse import urljoin
 from bs4 import BeautifulSoup
 
 from utils import send_to_telegram
-from constants import (HLTV_ORG, EXPIRE_AFTER, USER_AGENT_1, 
-                       USER_AGENT_2, USER_AGENT_3, USER_AGENT_4, 
-                       USER_AGENT_5, USER_AGENT_6)
+from constants import (HLTV_ORG, USER_AGENT_1, USER_AGENT_2, USER_AGENT_3, 
+                       USER_AGENT_4, USER_AGENT_5, USER_AGENT_6, USER_AGENT_7,
+                       USER_AGENT_8, USER_AGENT_9, USER_AGENT_10, USER_AGENT_11, 
+                       USER_AGENT_12)
 
 
 class Parse:
     def __init__(self, link) -> None:
         self.link = link
-        self.session = requests_cache.CachedSession(expire_after=EXPIRE_AFTER)
         self.user_agents = [
             USER_AGENT_1,
             USER_AGENT_2,
             USER_AGENT_3,
             USER_AGENT_4,
             USER_AGENT_5,
-            USER_AGENT_6
-            ]
+            USER_AGENT_6,
+            USER_AGENT_7,
+            USER_AGENT_8,
+            USER_AGENT_9,
+            USER_AGENT_10,
+            USER_AGENT_11,
+            USER_AGENT_12,
+        ]
         self.last_news = None
-
 
     async def fetch_news(self):
         async with aiohttp.ClientSession() as session:
             user_agent = random.choice(self.user_agents)
             headers = {
                 'User-Agent': user_agent
-                }
+            }
             async with session.get(self.link, headers=headers) as response:
                 response_text = await response.text()
                 soup = BeautifulSoup(response_text, features='lxml')
@@ -43,7 +47,6 @@ class Parse:
                     news_link = urljoin(HLTV_ORG, href)
                     news_links.add(news_link)
                 return news_links
-
 
     async def parse_today_news(self):
         while True:
@@ -67,7 +70,7 @@ if __name__ == '__main__':
         filename='main.log',
         encoding='utf-8'
     )
-    
+
     console_handler = logging.StreamHandler()
     console_handler.setLevel(logging.INFO)
     console_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
@@ -75,8 +78,9 @@ if __name__ == '__main__':
     root_logger = logging.getLogger()
     root_logger.addHandler(console_handler)
     logging.info('Парсер запущен!')
-    
+
     parser = Parse(HLTV_ORG)
     loop = asyncio.get_event_loop()
     loop.run_until_complete(parser.parse_today_news())
+
     
