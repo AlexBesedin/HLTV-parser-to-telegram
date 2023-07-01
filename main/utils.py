784d7a9e79e8dd832d71c4bb23e108e660ce114b
Alex Besedin <alebesedin@yandex.ru>
2023-07-01 15:01:49 +0300

    добавлен логер в utils.py

--- main/utils.py
+++ main/utils.py
@@ -1,5 +1,6 @@
 import telebot
 import os
+import logging
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -7,7 +8,19 @@ load_dotenv()
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('CHAT_ID')
 
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    filename='telegram.log',
+    encoding='utf-8'
+)
+logger = logging.getLogger(__name__)
 
 def send_to_telegram(message):
+    try:
         bot = telebot.TeleBot(TELEGRAM_TOKEN)
         bot.send_message(CHAT_ID, message)
+        logger.info(f"Message sent to Telegram: {message}")
+    except Exception as e:
+        logger.error(f"Failed to send message to Telegram: {e}")
+
